f9d5d7aa3de0e091c53d9b52df78970da7213582
Luis Manuel Brito <lsbrt4@gmail.com>
2025-05-12 15:49:40 -0400

    Copio el diseno del cv ingles

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,10 @@
 import { defineConfig } from 'astro/config';
-
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    envPrefix: 'VITE_',
+    define: {
+      'import.meta.env.VITE_EMAIL': JSON.stringify(process.env.VITE_EMAIL),
+      'import.meta.env.VITE_PHONE': JSON.stringify(process.env.VITE_PHONE),
+    },
+  },
+});
